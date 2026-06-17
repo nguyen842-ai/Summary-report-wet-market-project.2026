0bd6dffc19e832c62411ef248f0acf9f9182bda9
nguyen842-ai <nguyen842@gmail.com>
2026-06-17 15:21:07 +0700

    Add files via upload

---
+++ dbin.js
@@ -0,0 +1,20 @@
+// File: dbin.js
+// Đã chuyển đổi Qty và s sang kiểu Số (Number) để tính toán trực tiếp
+
+export const dbinSales = [
+  {hq: "2.North",ar: "1. Ha Noi",Pv: "Hà Nội" ,sku: "Miến tươi Song Long 1kg",mo: "May",Qty: 1562,s:412211800 },
+{hq: "2.North",ar: "1. Ha Noi",Pv: "Hà Nội" ,sku: "Miến tươi Song Long 3kg",mo: "May",Qty: 360,s:191646000 },
+{hq: "1.South",ar: "1. HCMC",Pv: "TP.Hồ Chí Minh" ,sku: "Miến đậu xanh Song Long 200g",mo: "May",Qty: 43,s:50086400 },
+{hq: "1.South",ar: "1. HCMC",Pv: "TP.Hồ Chí Minh" ,sku: "Miến tươi Song Long 1kg",mo: "May",Qty: 1707,s:450477300 },
+{hq: "1.South",ar: "1. HCMC",Pv: "TP.Hồ Chí Minh" ,sku: "Miến tươi Song Long 200g",mo: "May",Qty: 6,s:2402400 },
+{hq: "1.South",ar: "1. HCMC",Pv: "TP.Hồ Chí Minh" ,sku: "Miến tươi Song Long 3kg",mo: "May",Qty: 209,s:111261150 },
+{hq: "1.South",ar: "1. HCMC",Pv: "TP.Hồ Chí Minh" ,sku: "Miến tươi Song Long 450g",mo: "May",Qty: 23,s:9627800 },
+{hq: "1.South",ar: "1. HCMC",Pv: "TP.Hồ Chí Minh" ,sku: "Miến tươi Song Lân 500g",mo: "May",Qty: 14,s:7832743.1 },
+{hq: "1.South",ar: "6. Central",Pv: "Đà Nẵng" ,sku: "Miến tươi Song Long 1kg",mo: "May",Qty: 360,s:95004000 },
+{hq: "1.South",ar: "6. Central",Pv: "Đà Nẵng" ,sku: "Miến tươi Song Long 3kg",mo: "May",Qty: 80,s:42588000 },
+{hq: "1.South",ar: "4. Eastern",Pv: "Đồng Nai" ,sku: "Miến đậu xanh Song Long 200g",mo: "May",Qty: 20,s:23296000 },
+{hq: "1.South",ar: "4. Eastern",Pv: "TP.Hồ Chí Minh" ,sku: "Miến đậu xanh Song Long 200g",mo: "May",Qty: 10,s:11648000 },
+{hq: "1.South",ar: "4. Eastern",Pv: "Đồng Nai" ,sku: "Miến tươi Song Long 1kg",mo: "May",Qty: 230,s:60697000 },
+{hq: "1.South",ar: "4. Eastern",Pv: "Đồng Nai" ,sku: "Miến tươi Song Long 3kg",mo: "May",Qty: 56,s:29811600 },
+
+];
